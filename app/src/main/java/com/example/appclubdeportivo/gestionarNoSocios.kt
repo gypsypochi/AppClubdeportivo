396791276eead92218ab4b6c46cc4ac97346240d
PedroBriceño <pedroluisbd10@gmail.com>
2025-05-12 15:53:49 -0300

    Gestionar no Socios

---
+++ app/src/main/java/com/example/appclubdeportivo/gestionarNoSocios.kt
@@ -0,0 +1,20 @@
+package com.example.appclubdeportivo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class gestionarNoSocios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_gestionar_no_socios)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
