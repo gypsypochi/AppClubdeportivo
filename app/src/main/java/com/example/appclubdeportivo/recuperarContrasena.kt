2138b379f6d8dd6f4beabe150fd1e21d23c88851
veronica <veroguerra2292@gmail.com>
2025-05-13 22:35:01 -0300

    activity de receperar contraseña

--- app/src/main/java/com/example/appclubdeportivo/recuperarContrasena.kt
+++ app/src/main/java/com/example/appclubdeportivo/recuperarContrasena.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class GestionarSocios : AppCompatActivity() {
+class RecuperarContrasena : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_gestionar_socios)
+        setContentView(R.layout.activity_recuperar_contrasena)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
