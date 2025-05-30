ed867937069a828c2443e7e7783f86383614b219
PedroBriceño <pedroluisbd10@gmail.com>
2025-05-29 23:16:00 -0300

    dbHelper... Login funcionando

--- app/src/main/java/com/example/appclubdeportivo/ClubDBHelper.kt
+++ app/src/main/java/com/example/appclubdeportivo/ClubDBHelper.kt
@@ -4,35 +4,113 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class ClubDBHelper (context: Context): SQLiteOpenHelper(context, "ClubDB", null, 1) {
+class ClubDBHelper (context: Context): SQLiteOpenHelper(context, "ClubDB", null, 3) {
 
     override fun onCreate(db: SQLiteDatabase) {
+
+    // Crear las tablas
         db.execSQL("""
-            CREATE TABLE Direccion (
-            idDireccion INTENGER PRIMARY KEY AUTOINCREMENT,
+            CREATE TABLE direccion (
+            idDireccion INTEGER PRIMARY KEY AUTOINCREMENT,
             calle TEXT,
-            altura INTENGER,
+            altura INTEGER,
             barrio TEXT,
             localidad TEXT
             )
         """.trimIndent())
 
         db.execSQL("""
-            CREATE TABLE Socio (
-            idSocio INTENGER PRIMARY KEY AUTOINCREMENT,
+            CREATE TABLE socio (
+            idSocio INTEGER PRIMARY KEY AUTOINCREMENT,
             nombre TEXT,
             apellido TEXT,
-            dni INTENGER UNIQUE,
+            dni INTEGER UNIQUE,
             nacionalidad TEXT,
-            telefono INTENGER,
-            idDireccion INTENGER,
+            telefono INTEGER,
+            idDireccion INTEGER,
             venceCuota TEXT,
-            FOREIGN KEY (idDireccion) REFERENCES Direccion(idDireccion)
+            FOREIGN KEY (idDireccion) REFERENCES direccion(idDireccion)
             )
         """.trimIndent())
+
+        db.execSQL("""
+            CREATE TABLE noSocio (
+            idNoSocio INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT,
+            apellido TEXT,
+            dni INTEGER UNIQUE,
+            nacionalidad TEXT,
+            telefono INTEGER,
+            idDireccion INTEGER,
+            venceCuota TEXT,
+            FOREIGN KEY (idDireccion) REFERENCES direccion(idDireccion)
+            )
+        """.trimIndent())
+
+        db.execSQL("""
+            CREATE TABLE actividad (
+            idActividad INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT,
+            arancelMensual DOUBLE,
+            arancelDiario DOUBLE,
+            maxInscritos INTEGER
+            )
+        """.trimIndent())
+
+        db.execSQL("""
+            CREATE TABLE socioActividad (
+            idSocioActividad INTEGER PRIMARY KEY AUTOINCREMENT,
+            idSocio INTEGER,
+            idActividad INTEGER,
+            FOREIGN KEY (idSocio) REFERENCES socio(idSocio),
+            FOREIGN KEY (idActividad) REFERENCES actividad(idActividad)
+            )
+        """.trimIndent())
+
+        db.execSQL("""
+            CREATE TABLE noSocioActividad (
+            idNoSocioActividad INTEGER PRIMARY KEY AUTOINCREMENT,
+            idNoSocio INTEGER,
+            idActividad INTEGER,
+            FOREIGN KEY (idNoSocio) REFERENCES noSocio(idNoSocio),
+            FOREIGN KEY (idActividad) REFERENCES actividad(idActividad)
+            )
+        """.trimIndent())
+
+        db.execSQL("""
+            CREATE TABLE user (
+            idUser INTEGER PRIMARY KEY AUTOINCREMENT,
+            userName TEXT,
+            password TEXT
+            )
+        """.trimIndent())
+
+        db.execSQL("INSERT INTO user (userName, password) VALUES ('Veronica', '12345')")
+        db.execSQL("INSERT INTO user (userName, password) VALUES ('Micaela', '12123')")
+        db.execSQL("INSERT INTO user (userName, password) VALUES ('Pedro', '12321')")
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS user")
+        db.execSQL("DROP TABLE IF EXISTS noSocioActividad")
+        db.execSQL("DROP TABLE IF EXISTS socioActividad")
+        db.execSQL("DROP TABLE IF EXISTS actividad")
+        db.execSQL("DROP TABLE IF EXISTS noSocio")
+        db.execSQL("DROP TABLE IF EXISTS socio")
+        db.execSQL("DROP TABLE IF EXISTS direccion")
 
+        onCreate(db)
+    }
+
+    // Funcion Login
+    fun login(userName: String, password: String): Boolean {
+        val db = readableDatabase
+        val cursor = db.rawQuery(
+            "SELECT * FROM user WHERE userName=? AND password=?",
+            arrayOf(userName, password)
+        )
+        val exitoso = cursor.count > 0
+        cursor.close()
+        return exitoso
     }
 }
--- app/src/main/java/com/example/appclubdeportivo/LoginActivity.kt
+++ app/src/main/java/com/example/appclubdeportivo/LoginActivity.kt
@@ -5,19 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var dbHelper: ClubDBHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
+        dbHelper = ClubDBHelper(this)
+
+        val user = findViewById<EditText>(R.id.usernameEditText)
+        val pass = findViewById<EditText>(R.id.passwordEditText)
+
         /* Boton Logueo*/
         val btnLogin: Button =findViewById(R.id.btnLogin)
         btnLogin.setOnClickListener{
-            val intentar = Intent(this, MenuPrincipalActivity::class.java)
-            startActivity(intentar)
+            val u = user.text.toString().trim()
+            val p = pass.text.toString().trim()
+            if (dbHelper.login(u, p)) {
+                Toast.makeText(this, "Bienvenido $u", Toast.LENGTH_SHORT).show()
+                val intentar = Intent(this, MenuPrincipalActivity::class.java)
+                startActivity(intentar)
+            } else {
+                Toast.makeText(this, "Acceso Denegado", Toast.LENGTH_SHORT).show()
+            }
         }
 
         /* Boton Registro */
